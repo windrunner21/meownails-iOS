7b490fb580a07942a71816896ca1c271698d89b6
Imran <haciyev008@gmail.com>
2021-11-25 15:14:39 +0400

    Added confirmation UI and Animations

--- MeowNails/Modals/ContactInformationView.swift
+++ MeowNails/Modals/ContactInformationView.swift
@@ -11,90 +11,124 @@ struct ContactInformationView: View {
     @Binding var isPresented: Bool
     @State private var phoneNumber: String = "+994"
     @State private var fullName: String = String()
+    @State private var bookingCompleted: Bool = false
     
     var body: some View {
-        NavigationView {
-            VStack {
-                // Contact Information Prompt
-                HStack {
-                    Text("Enter your contact information")
-                        .foregroundColor(.gray)
-                        .fontWeight(.light)
-                    .textCase(.uppercase)
-                    
-                    Spacer()
-                }
-                
-                // Phone Number and Full Name TextFields
-                TextField("Phone Number", text: $phoneNumber)
-                    .keyboardType(.phonePad)
-                    .font(Font.headline.weight(.bold))
-                    .padding([.top])
-                Divider()
-                    .padding([.bottom])
-                TextField("Full Name", text: $fullName)
-                    .textInputAutocapitalization(.words)
-                    .font(Font.headline.weight(.regular))
-                Divider()
-                
-                // Review Order Prompt
-                HStack {
-                    Text("Review your order")
-                        .foregroundColor(.gray)
-                        .fontWeight(.light)
-                    .textCase(.uppercase)
-                    
-                    Spacer()
-                }
-                .padding([.top], 30)
-                
-                // Ordered and Price UI
+        ZStack {
+            NavigationView {
                 VStack {
-                    HStack(alignment: .bottom) {
-                        Text("You ordered:")
-                            .font(.system(size: 20, weight: .semibold))
+                    // Contact Information Prompt
+                    HStack {
+                        Text("Enter your contact information")
+                            .foregroundColor(.gray)
+                            .fontWeight(.light)
+                        .textCase(.uppercase)
+                        
                         Spacer()
-                        VStack(alignment: .trailing) {
-                            Text("Manicure")
-                            Text("Design")
-                        }
-                        .font(.system(size: 20, weight: .regular))
                     }
-                    .padding([.top])
+                    
+                    // Phone Number and Full Name TextFields
+                    TextField("Phone Number", text: $phoneNumber)
+                        .keyboardType(.phonePad)
+                        .font(Font.headline.weight(.bold))
+                        .padding([.top])
                     Divider()
-                    HStack(alignment: .bottom) {
-                        Text("Total Price:")
-                            .font(.system(size: 20, weight: .semibold))
+                        .padding([.bottom])
+                    TextField("Full Name", text: $fullName)
+                        .textInputAutocapitalization(.words)
+                        .font(Font.headline.weight(.regular))
+                    Divider()
+                    
+                    // Review Order Prompt
+                    HStack {
+                        Text("Review your order")
+                            .foregroundColor(.gray)
+                            .fontWeight(.light)
+                        .textCase(.uppercase)
+                        
                         Spacer()
-                        Text("30 ₼")
-                            .font(.system(size: 30, weight: .bold))
                     }
+                    .padding([.top], 30)
+                    
+                    // Ordered and Price UI
+                    VStack {
+                        HStack(alignment: .bottom) {
+                            Text("You ordered:")
+                                .font(.system(size: 20, weight: .semibold))
+                            Spacer()
+                            VStack(alignment: .trailing) {
+                                Text("Manicure")
+                                Text("Design")
+                            }
+                            .font(.system(size: 20, weight: .regular))
+                        }
+                        .padding([.top])
+                        Divider()
+                        HStack(alignment: .bottom) {
+                            Text("Total Price:")
+                                .font(.system(size: 20, weight: .semibold))
+                            Spacer()
+                            Text("30 ₼")
+                                .font(.system(size: 30, weight: .bold))
+                        }
+                    }
+                    
+                    // Complete Booking Button
+                    Button(action: {
+                        // animation for transition
+                        withAnimation(.easeInOut) {
+                            bookingCompleted = true
+                        }
+                        
+                        // close activity after 2 seconds
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            isPresented = false
+                        }
+                        
+                    }) {
+                        Text("Confirm Booking")
+                            .font(.system(size: 20, weight: .semibold))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding([.top])
+                    
+                    Spacer()
                 }
-                
-                // Complete Booking Button
-                Button(action: {
-                    isPresented = false
-                }) {
-                    Text("Complete")
-                        .font(.system(size: 20, weight: .semibold))
-                        .frame(maxWidth: .infinity)
-                }
-                .foregroundColor(.white)
                 .padding()
-                .background(Color.blue)
-                .cornerRadius(10)
-                .padding([.top])
-                
-                Spacer()
-            }
-            .padding()
-            .navigationTitle("Review & Book")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Cancel") {
-                        isPresented = false
+                .navigationTitle("Review & Book")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            isPresented = false
+                        }
                     }
                 }
+                
+            }
+            .blur(radius: bookingCompleted ? 2 : 0)
+            .animation(.linear, value: bookingCompleted)
+            .disabled(bookingCompleted)
+            .zIndex(1)
+            
+            if bookingCompleted {
+                VStack {
+                    Image(systemName: "checkmark")
+                        .foregroundColor(.blue)
+                        .font(.system(size: 80))
+                        .padding()
+                    Text("Booking successful")
+                }
+                .zIndex(2)
+                .transition(.scale)
+                .padding(40)
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .padding()
+                .shadow(radius: 10)
             }
         }
     }
